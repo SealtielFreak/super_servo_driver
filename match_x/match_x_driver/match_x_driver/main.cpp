08e0a76cd82f68e70433e8d97f63e1e6a4fddcde
SealtielFreak <Die.Seal_Valderrama@outlook.com>
2024-05-10 00:48:18 -0600

    Improved model simulator

---
+++ match_x/match_x_driver/match_x_driver/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * match_x_driver.cpp
+ *
+ * Created: 09/05/2024 09:29:17 p. m.
+ * Author : Sealtiel
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
