2c1eac539d8c2c938ddda706f90c9b958029702d
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-05-22 17:44:55 -0600

    First commit of driver controller system in C/C++

---
+++ match_x/driver_controller_code/DriverController/DriverController/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * DriverController.cpp
+ *
+ * Created: 22/05/2024 05:43:36 p. m.
+ * Author : Sealtiel
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
